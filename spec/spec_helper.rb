3b7164773cd07fedfe9b66a2f47217948109c484
Vinh Luu <vinhlt3@gmail.com>
2018-05-01 17:02:25 +0700

    Implement test cases

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,9 @@
 require 'pry'
 
-ordered_directories = %w(libs helpers models **)
+ordered_directories = %w(libs)
 ordered_directories.each do |directory|
   Dir[Dir.pwd + "/app/#{directory}/*.rb"].each { |file| require file }
 end
 
-TaxCalculation.new.run
+require 'fileutils'
+include CsvHelper
